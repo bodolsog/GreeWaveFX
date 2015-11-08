306093b24248aa1938a3996a6259ac6443d4aeec
Paweł B.B. Drozd <bodolsog@gmail.com>
2015-11-08 22:18:04 +0100

    Create PropertiesManager - class for managing config.properties file - and add getGoogleAPIKey method.

---
+++ src/pl/bodolsog/GreenWaveFX/PropertiesManager.java
@@ -0,0 +1,37 @@
+package pl.bodolsog.GreenWaveFX;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * This class handle properties from config.properties file.
+ *
+ * @author Paweł B.B. Drozd
+ *
+ */
+public class PropertiesManager {
+
+    private Properties properties;
+
+    /**
+     * Constructor create properties object from stream to config.properties file.
+     */
+    public PropertiesManager(){
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            System.out.println("Sorry, could not load config file.");
+        }
+    }
+
+    /**
+     * Read from config file Google API key.
+     * @return Google API key
+     */
+    public String getGoogleAPIKey(){
+        return properties.getProperty("googleAPIKey");
+    }
+
+}
