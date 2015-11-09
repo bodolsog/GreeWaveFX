93f37f29987bc62bf02ba2c90cc20f9b1b224ac9
Paweł B.B. Drozd <bodolsog@gmail.com>
2015-11-10 00:47:36 +0100

    Add Markers class for manage markers in JX

---
+++ src/pl/bodolsog/GreenWaveFX/tools/Markers.java
@@ -0,0 +1,19 @@
+package pl.bodolsog.GreenWaveFX.tools;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Markers {
+    private double lat;
+    private double lng;
+
+    public void setLatLng(double lat, double lng){
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public String getLatLng(){
+        return "lat: "+String.valueOf(lat)+", lng: "+String.valueOf(lng);
+    }
+}
