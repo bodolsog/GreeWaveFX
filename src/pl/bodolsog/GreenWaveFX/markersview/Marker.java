62913ff7590bf44f5c1c5e859d4951488207365c
Paweł B.B. Drozd <bodolsog@gmail.com>
2015-11-11 22:24:28 +0100

    Refactor Markers (model) to Marker.

---
+++ src/pl/bodolsog/GreenWaveFX/markersview/Marker.java
@@ -0,0 +1,43 @@
+package pl.bodolsog.GreenWaveFX.markersview;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class Marker {
+    private DoubleProperty lat;
+    private DoubleProperty lng;
+
+    /**
+     * Constructor.
+     * @param lat   latitude
+     * @param lng   longitude
+     */
+    public Marker(double lat, double lng){
+        this.lat = new SimpleDoubleProperty(lat);
+        this.lng = new SimpleDoubleProperty(lng);
+    }
+
+    /**
+     * Returns latitude as DoubleProperty
+     * @return
+     */
+    public DoubleProperty latProperty(){
+        return lat;
+    }
+
+    /**
+     * Returns longitude as DoubleProperty
+     * @return
+     */
+    public DoubleProperty lngProperty(){
+        return lng;
+    }
+
+    /**
+     * Test function
+     * @return String "lat: {lat}, lng: {lng}"
+     */
+    public String getLatLng(){
+        return "lat: "+String.valueOf(lat.get())+", lng: "+String.valueOf(lng.get());
+    }
+}
