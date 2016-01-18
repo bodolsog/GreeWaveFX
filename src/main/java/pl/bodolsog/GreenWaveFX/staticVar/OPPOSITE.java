1b69b9404e57514aa617b227bb5844a268d87895
Pawel B.B. Drozd <bodolsog@gmail.com>
2016-01-18 12:35:38 +0100

    Change static to OPPOSITE with Map.

--- src/main/java/pl/bodolsog/GreenWaveFX/staticVar/OPPOSITE.java
+++ src/main/java/pl/bodolsog/GreenWaveFX/staticVar/OPPOSITE.java
@@ -1,9 +1,11 @@
 package pl.bodolsog.GreenWaveFX.staticVar;
 
+import java.util.HashMap;
+
 /**
  * Created by bodolsog on 18.01.16.
  */
-public class OPPOSITE_DIRECTIONS {
+public class OPPOSITE {
     public static final String[] N = {DIRECTION.S, DIRECTION.SE, DIRECTION.SW};
     public static final String[] S = {DIRECTION.N, DIRECTION.NE, DIRECTION.NW};
     public static final String[] E = {DIRECTION.W, DIRECTION.NW, DIRECTION.SW};
@@ -12,4 +14,15 @@ public class OPPOSITE_DIRECTIONS {
     public static final String[] NE = {DIRECTION.SW, DIRECTION.S, DIRECTION.W};
     public static final String[] SW = {DIRECTION.NE, DIRECTION.N, DIRECTION.E};
     public static final String[] SE = {DIRECTION.NW, DIRECTION.N, DIRECTION.W};
+
+    public static final HashMap<String, String[]> DIRECTIONS = new HashMap<String, String[]>() {{
+        put(DIRECTION.N, N);
+        put(DIRECTION.S, S);
+        put(DIRECTION.E, E);
+        put(DIRECTION.W, W);
+        put(DIRECTION.NW, NW);
+        put(DIRECTION.NE, NE);
+        put(DIRECTION.SW, SW);
+        put(DIRECTION.SE, SE);
+    }};
 }
